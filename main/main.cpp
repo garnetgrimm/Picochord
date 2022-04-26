3b7ca9494717cd2d99196593e832571ba2918c1c
Garnet Grimm <garnetgrimm720@gmail.com>
2022-04-25 20:00:48 -0400

    floats!

--- main/main.cpp
+++ main/main.cpp
@@ -4,23 +4,21 @@
 #include "hardware/clocks.h"
 #include "hardware/pwm.h"
 
-typedef uint16_t u16;
-typedef uint32_t u32;
-
-void confPWM(int pin, u32 f_pwm, u16 duty) {
+void confPWM(int pin, float f_pwm, float duty) {
+	
 	gpio_set_function(pin, GPIO_FUNC_PWM); // Tell GPIO 0 it is allocated to the PWM
 	uint slice_num = pwm_gpio_to_slice_num(pin); // get PWM slice for GPIO 0 (it's slice 0)
 	
 	// set frequency
 	// determine top given Hz - assumes free-running counter rather than phase-correct
-	u32 f_sys = clock_get_hz(clk_sys); // typically 125'000'000 Hz
+	uint32_t f_sys = clock_get_hz(clk_sys); // typically 125'000'000 Hz
 	float divider = f_sys / 1000000UL;  // let's arbitrarily choose to run pwm clock at 1MHz
 	pwm_set_clkdiv(slice_num, divider); // pwm clock should now be running at 1MHz
-	u32 top =  1000000UL/f_pwm -1; // TOP is u16 has a max of 65535, being 65536 cycles
+	uint32_t top =  (uint32_t)(1000000.0f/f_pwm - 1.0f); // TOP is u16 has a max of 65535, being 65536 cycles
 	pwm_set_wrap(slice_num, top);
 
 	// set duty cycle
-	u16 level = (top+1) * duty / 100 -1; // calculate channel level from given duty cycle in %
+	uint16_t level = (uint16_t)(((float)top+1.0f) * duty - 1.0f); // calculate channel level from given duty cycle in %
 	pwm_set_chan_level(slice_num, 0, level); 
 	
 	pwm_set_enabled(slice_num, true); // let's go!
@@ -28,9 +26,9 @@ void confPWM(int pin, u32 f_pwm, u16 duty) {
 
 int main() 
 {
-	confPWM(0, 440, 50);
-	confPWM(2, 392, 50);
-	confPWM(4, 261, 50);
+	confPWM(0, 261.63f, 0.50f);
+	confPWM(2, 329.63f, 0.50f);
+	confPWM(4, 392.00f, 0.50f);
 	for(;;);
 	return 0;
 }
